4c3d663bcd6f2f987ea5466618387cb374aa035e
Yu-wen Pwu <ywpu@cs.nctu.edu.tw>
2017-01-12 15:51:42 +0800

    Add 5x5x5 leds support.

--- inc/74hc595.h
+++ inc/74hc595.h
@@ -6,7 +6,16 @@
 #define _74HC595_STCP_PIN GPIO_PIN_7
 #define _74HC595_DS_PIN GPIO_PIN_8
 
+#define _74HC595_DS0_PIN GPIO_PIN_8
+#define _74HC595_DS1_PIN GPIO_PIN_9
+#define _74HC595_DS2_PIN GPIO_PIN_10
+#define _74HC595_DS3_PIN GPIO_PIN_11
+#define LED_ON 1;
+#define LED_OFF 0;
+typedef uint8_t led_t;
+
 void _74hc595_init();
 void _74hc595_send(uint8_t);
+void _74hc595_cube_send(led_t[5][5][5]);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -2,11 +2,66 @@
 #include "stm32l4xx_nucleo.h"
 #include "74hc595.h"
 
+led_t leds[5][5][5] = {0};
+int state = 0;
+
 int main() {
-	uint8_t data = 0;
 	_74hc595_init();
 	while (1) {
+		switch (state) {
+		case 0:
+			for (int i = 0; i < 5; ++i)
+				for (int j = 0; j < 5; ++j)
+					leds[0][i][j] = LED_ON;
+			break;
+		case 1:
+			for (int i = 0; i < 5; ++i)
+				for (int j = 0; j < 5; ++j)
+					leds[1][i][j] = LED_ON;
+			break;
+		case 2:
+			for (int i = 0; i < 5; ++i)
+				for (int j = 0; j < 5; ++j)
+					leds[2][i][j] = LED_ON;
+			break;
+		case 3:
+			for (int i = 0; i < 5; ++i)
+				for (int j = 0; j < 5; ++j)
+					leds[3][i][j] = LED_ON;
+			break;
+		case 4:
+			for (int i = 0; i < 5; ++i)
+				for (int j = 0; j < 5; ++j)
+					leds[4][i][j] = LED_ON;
+			break;
+		case 5:
+			for (int i = 0; i < 5; ++i)
+				for (int j = 0; j < 5; ++j)
+					leds[0][i][j] = LED_OFF;
+			break;
+		case 6:
+			for (int i = 0; i < 5; ++i)
+				for (int j = 0; j < 5; ++j)
+					leds[1][i][j] = LED_OFF;
+			break;
+		case 7:
+			for (int i = 0; i < 5; ++i)
+				for (int j = 0; j < 5; ++j)
+					leds[2][i][j] = LED_OFF;
+			break;
+		case 8:
+			for (int i = 0; i < 5; ++i)
+				for (int j = 0; j < 5; ++j)
+					leds[3][i][j] = LED_OFF;
+			break;
+		case 9:
+			for (int i = 0; i < 5; ++i)
+				for (int j = 0; j < 5; ++j)
+					leds[4][i][j] = LED_OFF;
+			break;
+		}
+		state = state < 9 ? state + 1 : 0;
+		_74hc595_cube_send(leds);
 		HAL_Delay(1000);
-		_74hc595_send(data++);
 	}
 }
